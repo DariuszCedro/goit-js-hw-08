8596d6692804015ebb7797e6fcfc0862c7f78fde
DariuszCedro <dariuszcedro@gmail.com>
2023-08-19 19:57:36 +0200

    task-03 start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const form = document.querySelector('.feedback-form');
+const start = document.querySelector(`[name="email"]`);
+form.addEventListener('input', handleSubmit);
 
+function handleSubmit(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const obj = { email: email.value, message: message.value };
+  console.log(obj);
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}
