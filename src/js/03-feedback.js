b90743ffb5a3d104361075afda1a3023d88f4423
DariuszCedro <dariuszcedro@gmail.com>
2023-08-21 14:13:50 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,31 @@
+var throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
-const start = document.querySelector(`[name="email"]`);
-form.addEventListener('input', handleSubmit);
+const emailInput = document.querySelector(`[name="email"]`);
+const textInput = document.querySelector(`[name="message"]`);
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
+function handleInput(event) {
   const {
     elements: { email, message },
   } = event.currentTarget;
   const obj = { email: email.value, message: message.value };
-  console.log(obj);
   localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const objToReset = { email: '', message: '' };
+  localStorage.setItem('feedback-form-state', JSON.stringify(objToReset));
+  emailInput.value = '';
+  textInput.value = '';
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+}
+
+const dataFromLS = localStorage.getItem('feedback-form-state');
+const data = JSON.parse(dataFromLS);
+
+if (dataFromLS) {
+  emailInput.value = data.email;
+  textInput.value = data.message;
 }
