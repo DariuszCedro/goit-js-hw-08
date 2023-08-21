278215e24fb6baf47ef4d526659593e838990e4a
DariuszCedro <126669439+DariuszCedro@users.noreply.github.com>
2023-08-21 12:14:31 +0000

    Deploying to gh-pages from @ DariuszCedro/goit-js-hw-08@b90743ffb5a3d104361075afda1a3023d88f4423 🚀

--- 03-feedback.39c674ae.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");document.querySelector('[name="email"]');e.addEventListener("input",(function(e){var t=e.currentTarget.elements,a=t.email,n=t.message,r={email:a.value,message:n.value};console.log(r),localStorage.setItem("feedback-form-state",JSON.stringify(r))}))}();
-//# sourceMappingURL=03-feedback.39c674ae.js.map
